c2f1d993710756228589c21069cb251e6b9c3e81
Byron <53088136+byronbytes@users.noreply.github.com>
2021-12-25 13:34:09 -0500

    Fixed capitalization

--- Windows Activator/KeyType.vb
+++ Windows Activator/KeyType.vb
@@ -1,7 +1,7 @@
 ﻿Public Class KeyType
   Public Property Key As String = String.Empty
-  Public Property OsVErsion As String = String.Empty
-  Public Property OsCompilation As String = String.Empty
+    Public Property OsVersion As String = String.Empty
+    Public Property OsCompilation As String = String.Empty
   Public Property PC As String = String.Empty
 
 
