71722c67cf676cc1303f3f0df306941b7ed2a2ba
Byron <53088136+byronbytes@users.noreply.github.com>
2021-12-25 13:39:50 -0500

    code format update

--- Windows Activator/KeyType.vb
+++ Windows Activator/KeyType.vb
@@ -1,8 +1,7 @@
 ﻿Public Class KeyType
-  Public Property Key As String = String.Empty
+    Public Property Key As String = String.Empty
     Public Property OsVersion As String = String.Empty
     Public Property OsCompilation As String = String.Empty
-  Public Property PC As String = String.Empty
-
+    Public Property PC As String = String.Empty
 
 End Class
